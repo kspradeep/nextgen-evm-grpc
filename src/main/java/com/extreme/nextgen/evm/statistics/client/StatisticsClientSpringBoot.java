122d56e69b912314489591a15fbed272a6ebbc82
pkenkereshanthaveera <pkenkereshanthaveera@extremenetworks.com>
2020-01-21 11:18:45 +0530

    Commit new Grpc next gen evm project

--- src/main/java/com/extreme/nextgen/evm/statistics/client/StatisticsClientSpringBoot.java
+++ src/main/java/com/extreme/nextgen/evm/statistics/client/StatisticsClientSpringBoot.java
@@ -6,7 +6,6 @@ import com.proto.statistics.StatisticsResponse;
 import com.proto.statistics.StatisticsServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -15,24 +14,26 @@ import javax.annotation.PostConstruct;
 public class StatisticsClientSpringBoot {
 
     private StatisticsServiceGrpc.StatisticsServiceBlockingStub statisticsClient;
+
     @PostConstruct
-    private void init(){
+    private void init() {
         ManagedChannel channel = ManagedChannelBuilder
                 .forAddress("localhost", 50056).usePlaintext().build();
         // created a statistics service client (blocking - synchronous)
-        statisticsClient=StatisticsServiceGrpc.newBlockingStub(channel);
+        statisticsClient = StatisticsServiceGrpc.newBlockingStub(channel);
     }
-    public   String doUnaryCall(){
+
+    public String doUnaryCall() {
 
         // Unary
         // created a protocol buffer statistics message
 
-        Statistics statistics=Statistics.newBuilder().setFirstName("Pradeep").setLastName("Shanthaveerappa").build();
+        Statistics statistics = Statistics.newBuilder().setFirstName("Pradeep").setLastName("Shanthaveerappa").build();
         // do the same for a StatisticsRequest
-        StatisticsRequest statisticsRequest=StatisticsRequest.newBuilder().setStatistics(statistics).build();
+        StatisticsRequest statisticsRequest = StatisticsRequest.newBuilder().setStatistics(statistics).build();
 
         // call the RPC and get back a StatisticsResponse (protocol buffers)
-        StatisticsResponse statisticsResponse=statisticsClient.statistics(statisticsRequest);
+        StatisticsResponse statisticsResponse = statisticsClient.statistics(statisticsRequest);
         System.out.println(statisticsResponse.getResult());
         return statisticsResponse.getResult();
     }
