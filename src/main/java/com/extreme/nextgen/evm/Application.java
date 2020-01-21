2e4949620e1619ec0054c5cb1af1a7f6e6976eb1
pkenkereshanthaveera <pkenkereshanthaveera@extremenetworks.com>
2020-01-21 12:26:37 +0530

    Moved the Application file

--- src/main/java/com/extreme/nextgen/evm/Application.java
+++ src/main/java/com/extreme/nextgen/evm/Application.java
@@ -1,4 +1,4 @@
-package com.extreme.nextgen.evm.statistics;
+package com.extreme.nextgen.evm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
